a514f41b8b063ef82929b242af84c439dde5086f
anggagumilang212 <anggagumilang212@gmail.com>
2024-03-28 08:52:35 +0700

    api wilayah

---
+++ database/migrations/2024_03_28_010642_create_kelurahans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kelurahans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('kecamatan_id');
+            $table->integer('pos_code');
+            $table->integer('code')->nullable();
+            $table->integer('full_code')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kelurahans');
+    }
+};
